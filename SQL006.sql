5c368b67fc7bb26d250c87907b16241999cffebb
Bitts <marcelo.valvassori@gmail.com>
2021-03-29 15:01:24 -0300

    Create SQL006.sql

---
+++ SQL006.sql
@@ -0,0 +1,22 @@
+/*
+	PLT019 - Função para limpar um número ClearNumber - cNumber
+	Create By Bitts
+	(13/11/2015)
+	
+	UTILIZAÇÃO: SELECT dbo.cNumber(E.TELEFONE1) FROM PPESSOA
+
+*/
+
+CREATE FUNCTION [dbo].[cNumber](@Resultado VARCHAR(8000)) RETURNS VARCHAR(8000) AS
+BEGIN
+	DECLARE @CharInvalido SMALLINT
+	SET @CharInvalido = PATINDEX('%[^0-9]%', @Resultado)
+	WHILE @CharInvalido > 0
+		BEGIN
+			SET @Resultado = STUFF(@Resultado, @CharInvalido, 1, '')
+			SET @CharInvalido = PATINDEX('%[^0-9]%', @Resultado)
+		END
+	SET @Resultado = @Resultado
+	RETURN @Resultado
+END;
+
